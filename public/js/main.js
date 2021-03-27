594219f12480fa186754294d0deef07272031986
Kunal Khullar <kunalkhullar5012@gmail.com>
2021-03-28 02:17:53 +0530

    animations

---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+function nextform()
+{
+
+    document.getElementById('userdetails2').classList.remove('hide');
+    document.getElementById('userdetails1').classList.add('hide');
+    document.getElementById('userdetails2').classList.add('animate__animated');
+    document.getElementById('userdetails2').classList.add('animate__fadeIn');
+}
