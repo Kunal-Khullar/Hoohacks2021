985095a2c76d11af92e18d42d4a7e0343393c57b
Kunal Khullar <kunalkhullar5012@gmail.com>
2021-03-28 12:48:11 +0530

    home page added

--- public/js/main.js
+++ public/js/main.js
@@ -6,3 +6,16 @@ function nextform()
     document.getElementById('userdetails2').classList.add('animate__animated');
     document.getElementById('userdetails2').classList.add('animate__fadeIn');
 }
+function register()
+{
+    console.log(document.getElementById('age1').value)
+    console.log(document.getElementById('weight1').value)
+    console.log(document.getElementById('height1').value)
+    console.log(document.getElementById('exampleInputEmail1').value)
+    console.log(document.getElementById('exampleInputName1').value)
+}
+function login(){
+    console.log(document.getElementById('exampleInputName2').value)
+    console.log(document.getElementById('exampleInputPassword2').value)
+    window.location.href ='home.html';
+}
